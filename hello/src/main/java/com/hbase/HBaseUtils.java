17dcca8e090bb183ec13ca91156c2b3816d57119
huangcx <623363564@qq.com>
2024-07-05 09:59:48 +0800

    将connection修改为实例变量

--- hello/src/main/java/com/hbase/HBaseUtils.java
+++ hello/src/main/java/com/hbase/HBaseUtils.java
@@ -1,5 +1,6 @@
 package com.hbase;
 
+import com.alibaba.fastjson2.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.*;
@@ -16,39 +17,35 @@ import java.util.List;
  */
 @Slf4j
 public class HBaseUtils {
-    private static Connection connection;
-    
     /**
-     * 获取HBase连接
+     * 创建HBase连接
      *
      * @return HBase连接
      */
-    public static Connection getConnection() {
-        if (connection == null || connection.isClosed()) {
-            Configuration conf = new Configuration();
-            conf.set("hbase.zookeeper.quorum", "192.168.100.100");
-            try {
-                connection = ConnectionFactory.createConnection(conf);
-                log.info("创建HBase连接成功！");
-                return connection;
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+    public static Connection createConnection() {
+        Configuration conf = new Configuration();
+        conf.set("hbase.zookeeper.quorum", "192.168.100.100");
+        try {
+            return ConnectionFactory.createConnection(conf);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("创建HBase连接失败！");
         }
-        log.info("获取HBase连接成功！");
-        return connection;
     }
     
     /**
      * 关闭HBase连接
+     *
+     * @param connection HBase连接
      */
-    public static void closeConnection() {
+    public static void closeConnection(Connection connection) {
         if (connection != null) {
             try {
                 connection.close();
                 log.info("关闭HBase连接成功！");
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
+                throw new RuntimeException("关闭HBase连接失败！");
             }
         }
     }
@@ -58,10 +55,11 @@ public class HBaseUtils {
     /**
      * 判断命名空间是否存在
      *
-     * @param namespace 命名空间
+     * @param connection HBase连接
+     * @param namespace  命名空间
      * @return 表格是否存在
      */
-    public static boolean isNamespaceExist(String namespace) {
+    public static boolean isNamespaceExist(Connection connection, String namespace) {
         try (Admin admin = connection.getAdmin()) {
             try {
                 admin.getNamespaceDescriptor(namespace);
@@ -79,11 +77,12 @@ public class HBaseUtils {
     /**
      * 判断表格是否存在
      *
-     * @param namespace 命名空间
-     * @param table     表格
+     * @param connection HBase连接
+     * @param namespace  命名空间
+     * @param table      表格
      * @return 表格是否存在
      */
-    public static boolean isTableExist(String namespace, String table) {
+    public static boolean isTableExist(Connection connection, String namespace, String table) {
         try (Admin admin = connection.getAdmin()) {
             return admin.tableExists(TableName.valueOf(namespace, table));
         } catch (IOException e) {
@@ -94,9 +93,10 @@ public class HBaseUtils {
     /**
      * 创建命名空间
      *
-     * @param namespace 命名空间名称
+     * @param connection HBase连接
+     * @param namespace  命名空间
      */
-    public static void createNamespace(String namespace) {
+    public static void createNamespace(Connection connection, String namespace) {
         try (Admin admin = connection.getAdmin()) {
             admin.createNamespace(NamespaceDescriptor.create(namespace).build());
         } catch (IOException e) {
@@ -107,17 +107,18 @@ public class HBaseUtils {
     /**
      * 创建表格
      *
+     * @param connection     HBase连接
      * @param namespace      命名空间
      * @param table          表格
      * @param columnFamilies 列族
      */
-    public static void createTable(String namespace, String table, String... columnFamilies) {
+    public static void createTable(Connection connection, String namespace, String table, String... columnFamilies) {
         if (columnFamilies.length == 0) {
-            throw new RuntimeException("表格至少需要一个列族");
+            throw new RuntimeException("表格至少需要一个列族！");
         }
         try (Admin admin = connection.getAdmin()) {
-            if (!isTableExist(namespace, table)) {
-                createNamespace(namespace);
+            if (!isTableExist(connection, namespace, table)) {
+                createNamespace(connection, namespace);
             }
             TableDescriptorBuilder tableDescriptorBuilder = TableDescriptorBuilder.newBuilder(TableName.valueOf(namespace, table));
             for (String columnFamily : columnFamilies) {
@@ -134,14 +135,15 @@ public class HBaseUtils {
     /**
      * 修改表格
      *
+     * @param connection   HBase连接
      * @param namespace    命名空间
      * @param table        表格
      * @param columnFamily 列族
      * @param maxVersion   最大版本
      */
-    public static void modifyTable(String namespace, String table, String columnFamily, int maxVersion) {
-        if (!isTableExist(namespace, table)) {
-            throw new RuntimeException("表格不存在");
+    public static void modifyTable(Connection connection, String namespace, String table, String columnFamily, int maxVersion) {
+        if (!isTableExist(connection, namespace, table)) {
+            throw new RuntimeException("表格不存在！");
         }
         try (Admin admin = connection.getAdmin()) {
             TableDescriptor tableDescriptor = admin.getDescriptor(TableName.valueOf(namespace, table));
@@ -159,13 +161,14 @@ public class HBaseUtils {
     /**
      * 删除表格
      *
-     * @param namespace 命令空间
-     * @param table     表格
+     * @param connection HBase连接
+     * @param namespace  命令空间
+     * @param table      表格
      * @return 是否删除成功
      */
-    public static boolean deleteTable(String namespace, String table) {
-        if (!isTableExist(namespace, table)) {
-            throw new RuntimeException("表格不存在");
+    public static boolean deleteTable(Connection connection, String namespace, String table) {
+        if (!isTableExist(connection, namespace, table)) {
+            throw new RuntimeException("表格不存在！");
         }
         try (Admin admin = connection.getAdmin()) {
             try {
@@ -188,6 +191,7 @@ public class HBaseUtils {
     /**
      * 写入单元格
      *
+     * @param connection      HBase连接
      * @param namespace       命名空间
      * @param table           表格
      * @param rowKey          RowKey
@@ -195,9 +199,9 @@ public class HBaseUtils {
      * @param columnQualifier 列限定符
      * @param value           数据
      */
-    public static void put(String namespace, String table, String rowKey, String columnFamily, String columnQualifier, String value) {
-        if (!isTableExist(namespace, table)) {
-            throw new RuntimeException("表格不存在");
+    public static void put(Connection connection, String namespace, String table, String rowKey, String columnFamily, String columnQualifier, String value) {
+        if (!isTableExist(connection, namespace, table)) {
+            throw new RuntimeException("表格不存在！");
         }
         try (Table t = connection.getTable(TableName.valueOf(namespace, table))) {
             Put put = new Put(rowKey.getBytes(StandardCharsets.UTF_8));
@@ -209,8 +213,34 @@ public class HBaseUtils {
     }
     
     /**
+     * 写入单元格
+     *
+     * @param connection   HBase连接
+     * @param namespace    命名空间
+     * @param table        表格
+     * @param rowKey       RowKey
+     * @param columnFamily 列族
+     * @param data         键值对数据
+     */
+    public static void put(Connection connection, String namespace, String table, String rowKey, String columnFamily, JSONObject data) {
+        if (!isTableExist(connection, namespace, table)) {
+            throw new RuntimeException("表格不存在！");
+        }
+        try (Table t = connection.getTable(TableName.valueOf(namespace, table))) {
+            Put put = new Put(rowKey.getBytes(StandardCharsets.UTF_8));
+            for (String columnQualifier : data.keySet()) {
+                put.addColumn(columnFamily.getBytes(StandardCharsets.UTF_8), columnQualifier.getBytes(StandardCharsets.UTF_8), data.getString(columnQualifier).getBytes(StandardCharsets.UTF_8));
+            }
+            t.put(put);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    /**
      * 读取单元格
      *
+     * @param connection      HBase连接
      * @param namespace       命名空间
      * @param table           表格
      * @param rowKey          RowKey
@@ -218,9 +248,9 @@ public class HBaseUtils {
      * @param columnQualifier 列限定符
      * @return 数据列表
      */
-    public static List<String> get(String namespace, String table, String rowKey, String columnFamily, String columnQualifier) {
-        if (!isTableExist(namespace, table)) {
-            throw new RuntimeException("表格不存在");
+    public static List<String> get(Connection connection, String namespace, String table, String rowKey, String columnFamily, String columnQualifier) {
+        if (!isTableExist(connection, namespace, table)) {
+            throw new RuntimeException("表格不存在！");
         }
         try (Table t = connection.getTable(TableName.valueOf(namespace, table))) {
             Get get = new Get(rowKey.getBytes(StandardCharsets.UTF_8));
@@ -241,15 +271,16 @@ public class HBaseUtils {
     /**
      * 删除单元格
      *
+     * @param connection      HBase连接
      * @param namespace       命名空间
      * @param table           表格
      * @param rowKey          RowKey
      * @param columnFamily    列族
      * @param columnQualifier 列限定符
      */
-    public static void delete(String namespace, String table, String rowKey, String columnFamily, String columnQualifier) {
-        if (!isTableExist(namespace, table)) {
-            throw new RuntimeException("表格不存在");
+    public static void delete(Connection connection, String namespace, String table, String rowKey, String columnFamily, String columnQualifier) {
+        if (!isTableExist(connection, namespace, table)) {
+            throw new RuntimeException("表格不存在！");
         }
         try (Table t = connection.getTable(TableName.valueOf(namespace, table))) {
             Delete delete = new Delete(rowKey.getBytes(StandardCharsets.UTF_8));
@@ -260,19 +291,39 @@ public class HBaseUtils {
         }
     }
     
+    /**
+     * 删除一行单元格
+     *
+     * @param connection HBase连接
+     * @param namespace  命名空间
+     * @param table      表格
+     * @param rowKey     RowKey
+     */
+    public static void delete(Connection connection, String namespace, String table, String rowKey) {
+        if (!isTableExist(connection, namespace, table)) {
+            throw new RuntimeException("表格不存在！");
+        }
+        try (Table t = connection.getTable(TableName.valueOf(namespace, table))) {
+            Delete delete = new Delete(rowKey.getBytes(StandardCharsets.UTF_8));
+            t.delete(delete);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     
     /**
      * 扫描表格
      *
-     * @param namespace 命名空间
-     * @param table     表格
-     * @param startRow  startRow
-     * @param stopRow   stopRow
+     * @param connection HBase连接
+     * @param namespace  命名空间
+     * @param table      表格
+     * @param startRow   startRow
+     * @param stopRow    stopRow
      * @return 数据列表
      */
-    public static List<String> scan(String namespace, String table, String startRow, String stopRow) {
-        if (!isTableExist(namespace, table)) {
-            throw new RuntimeException("表格不存在");
+    public static List<String> scan(Connection connection, String namespace, String table, String startRow, String stopRow) {
+        if (!isTableExist(connection, namespace, table)) {
+            throw new RuntimeException("表格不存在！");
         }
         try (Table t = connection.getTable(TableName.valueOf(namespace, table))) {
             Scan scan = new Scan();
@@ -292,6 +343,7 @@ public class HBaseUtils {
     /**
      * 扫描表格
      *
+     * @param connection      HBase连接
      * @param namespace       命名空间
      * @param table           表格
      * @param columnFamily    列族
@@ -301,9 +353,9 @@ public class HBaseUtils {
      * @param stopRow         stopRow
      * @return 数据列表
      */
-    public static List<String> filterScan(String namespace, String table, String columnFamily, String columnQualifier, String value, String startRow, String stopRow) {
-        if (!isTableExist(namespace, table)) {
-            throw new RuntimeException("表格不存在");
+    public static List<String> filterScan(Connection connection, String namespace, String table, String columnFamily, String columnQualifier, String value, String startRow, String stopRow) {
+        if (!isTableExist(connection, namespace, table)) {
+            throw new RuntimeException("表格不存在！");
         }
         try (Table t = connection.getTable(TableName.valueOf(namespace, table))) {
             Scan scan = new Scan();
--- hello/src/main/java/com/hbase/Main.java
+++ hello/src/main/java/com/hbase/Main.java
@@ -2,22 +2,6 @@ package com.hbase;
 
 public class Main {
     public static void main(String[] args) {
-        // HBaseUtils.deleteTable("test", "testTable");
-        // HBaseUtils.createTable("test", "testTable", "info");
-        // HBaseUtils.put("test", "testTable", "1000", "info", "name", "zs");
-        
-        // List<String> list = HBaseUtils.get("test", "testTable", "1000", "info", "name");
-        // list.forEach(System.out::println);
-        
-        // List<String> list = HBaseUtils.scan("test", "testTable", "0", "10000");
-        // list.forEach(System.out::println);
-        // HBaseUtils.delete("test", "testTable", "1000", "info", "name");
-        //
-        
-        System.out.println(HBaseUtils.getConnection());
-        HBaseUtils.closeConnection();
-        System.out.println(HBaseUtils.getConnection());
-        HBaseUtils.createTable("test", "testTable111", "info");
-        System.out.println(HBaseUtils.getConnection());
+    
     }
 }
